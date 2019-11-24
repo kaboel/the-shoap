585bf0f3360f9c2ef58eafa4d745be59d670af12
Faiq Allam <faiq.kaboel@gmail.com>
2019-11-24 10:33:10 +0700

    Setup database connection

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const App = express();
 
@@ -11,13 +11,26 @@ App.use(cors());
 App.use(morgan('combined'));
 App.use(bodyParser.urlencoded({extended: true}));
 
+require('./routes')(App);
+
+const dbUri = config.db.uriString;
 try {
-  const port = process.env.PORT || config.port;
-  App.listen(port, () => {
-    console.log(`Server started on port: ${port}...`);
+  mongoose.connect(dbUri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
   });
+  console.log('Database connected...');
+
+  try {
+    const port = process.env.PORT || config.port;
+    App.listen(port, () => {
+      console.log(`Server started on port: ${port}...`);
+    });
+  } catch (e) {
+    console.log('errcode(0): Server failed to start.');
+  }
 } catch (e) {
-  console.log('errcode(0): Server failed to start.');
+  console.log(`errcode(1): ${e.message}`);
 }
 
 
