3aad9fec1728a44ab8703e612cc0a2adee28dad4
Faiq Allam <faiq.kaboel@gmail.com>
2019-11-24 09:23:48 +0700

    add user model

---
+++ server/src/_models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const SCHEMA = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const MODEL = mongoose.model('User', SCHEMA, 'Users');
+
+module.exports = MODEL;
