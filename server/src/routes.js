60cfd36f1e26aeebe911968ddb0668b46d535e0e
Faiq Allam <faiq.kaboel@gmail.com>
2019-11-24 19:17:11 +0700

    delete regular Order model

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,8 +2,10 @@ const Order = require('./_controllers/OrderController');
 const Product = require('./_controllers/ProductController');
 
 module.exports = (App) => {
+  // Order End-point
   App.post('/v0/orders', Order.store);
 
+  // Product End-point
   App.get('/v0/products', Product.index);
   App.post('/v0/products', Product.store);
 };
