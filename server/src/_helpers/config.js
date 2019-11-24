1709a74e2e34f84d360bbe829594aa823450c12f
Faiq Allam <faiq.kaboel@gmail.com>
2019-11-24 10:34:36 +0700

    server .gitignore

--- server/src/_helpers/config.js
+++ server/src/_helpers/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    name: "shoap-server",
-    port: "8089",
-    db: {}
+  name: "shoap-server",
+  port: "8089",
+  db: {
+    uriString: `mongodb+srv://shoap:Ff456217@kluster-0-co84f.mongodb.net/shoap?retryWrites=true&w=majority`
+  }
 };
