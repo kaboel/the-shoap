3e062ad829a32dbe9c8a76200472c650ef56caf2
Faiq Allam <faiq.kaboel@gmail.com>
2019-11-24 20:39:45 +0700

    fix eslint adapt

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Buefy from 'buefy'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(Buefy, {
+  defaultIconComponent: 'font-awesome-icon',
+  defaultIconPack: 'fas'
+})
 
 /* eslint-disable no-new */
 new Vue({
