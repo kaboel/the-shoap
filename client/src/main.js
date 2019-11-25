cc10d0d8059f0490d12f1d82ca79698b2510a13f
Faiq Allam <faiq.kaboel@gmail.com>
2019-11-26 03:02:26 +0700

    hook vuex to app

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import Buefy from 'buefy'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
@@ -19,6 +20,7 @@ Vue.use(Buefy, {
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
