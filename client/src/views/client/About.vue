47a382f1640fd8d40cb1f2bca0185bbe501a8172
Faiq Allam <faiq.kaboel@gmail.com>
2019-11-26 04:57:46 +0700

    initial about page style

--- client/src/views/client/About.vue
+++ client/src/views/client/About.vue
@@ -1,8 +1,19 @@
 <template>
+  <div>
+    <div class="columns">
+      <div class="column is-half">
+        <p class="title is-size-3">About The Shoap</p>
+      </div>
+      <div class="column is-half">
 
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-
+export default {
+  name: 'About'
+}
 </script>
 <style scoped>
 
