572b4815b80b040e4a2880262951f414a559e436
Faiq Allam <faiq.kaboel@gmail.com>
2019-11-26 07:01:31 +0700

    setup routing with vuex

--- client/src/components/client/TsNavbar.vue
+++ client/src/components/client/TsNavbar.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="navbar is-fixed-top is-spaced is-transparent" role="navigation" aria-label="main navigation">
+  <nav ref="navBar" class="navbar is-fixed-top is-spaced is-transparent" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="/">
+      <a class="navbar-item" href="#" @click="contentOff">
         <img src="../../assets/logo.png" class="logo">
       </a>
 
@@ -17,16 +17,13 @@
 
     <div id="navbarMenu" class="navbar-menu" :class="{'is-active' : navToggle}">
       <div class="navbar-end">
-        <a href="/about" class="navbar-link is-arrowless">
+        <a href="#" @click="routeTo('About')" class="navbar-link is-arrowless">
           About
         </a>
-        <a href="#projects" class="navbar-link is-arrowless">
+        <a href="#" @click="routeTo('Products')" class="navbar-link is-arrowless">
           Products
         </a>
-<!--        <a href="#projects" class="navbar-link is-arrowless">-->
-<!--          Store-->
-<!--        </a>-->
-        <a href="#projects" class="navbar-link is-arrowless">
+        <a href="#" @click="routeTo('Contact')" class="navbar-link is-arrowless">
           Contact
         </a>
         <div class="navbar-item has-text-centered">
@@ -47,7 +44,20 @@
 
 <script>
 export default {
-  name: 'TsNavbar'
+  name: 'TsNavbar',
+  data() {
+    return {
+      navToggle: false
+    }
+  },
+  methods: {
+    contentOff() {
+      this.$store.dispatch('contentOff')
+    },
+    routeTo(page) {
+      this.$store.dispatch('routeTo', page)
+    }
+  }
 }
 </script>
 
@@ -55,11 +65,7 @@ export default {
 .navbar {
   background: transparent !important;
   padding: 25px 50px 25px 50px;
-}
-html:not([data-scroll='0']) {
-  .navbar {
-    background-color: initial;
-  }
+  z-index: 9;
 }
 .button.is-outlined {
   color: #00a2ea !important;
@@ -79,5 +85,29 @@ html:not([data-scroll='0']) {
 .logo {
   width: 100px !important;
   max-height: 200px !important;
+}
+@media(max-width: 1024px) {
+  .navbar-menu.is-active {
+    background-color: #f6f6f6 !important;
+    border-top: 0;
+    border-bottom: 1px;
+    border-left: 0px;
+    border-right: 0px;
+    border-style: solid;
+    border-bottom-right-radius: 2rem;
+    border-bottom-left-radius: 2rem;
+    border-color: #ddd;
+    padding-left: .5rem !important;
+    padding-right: .5rem !important;
+    padding-bottom: 1rem !important;
+  }
+  .navbar-burger.is-active {
+    background-color: #f6f6f6 !important;
+  }
+  .navbar-menu.is-active a.navbar-item:hover,
+  .navbar-menu.is-active a.navbar-link:hover {
+    background-color: initial;
+    color: #0A0A0A;
+  }
 }
 </style>
