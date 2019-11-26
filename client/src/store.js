96ae125d8654f4ab7acb36eb58b9a06522fdd5bc
Faiq Allam <faiq.kaboel@gmail.com>
2019-11-26 07:02:03 +0700

    configure routing and content watcher mutations and actions

--- client/src/store.js
+++ client/src/store.js
@@ -6,12 +6,34 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     products: {},
-    cart: []
+    cart: [],
+    contentOn: false,
+    pageActive: null
   },
   mutations: {
+    contentOff(state) {
+      state.contentOn = false
+    },
+    toggleContent(state) {
+      (state.contentOn === true) ?
+        state.contentOn = false : state.contentOn = true
+    },
+    routeTo(state, page) {
+      state.contentOn = true
+      state.pageActive = page
+    }
+  },
+  actions: {
+    contentOff({commit}) {
+      commit('contentOff')
+    },
+    toggleContent({commit}) {
+      commit('toggleContent')
+    },
+    routeTo({commit}, page) {
+      commit('routeTo', page)
+    }
   }
 })
 
-export default {
-  store
-}
+export default store
