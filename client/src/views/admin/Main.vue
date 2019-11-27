f2d3bcd15c61616d41015ab7bdf1affe4682bce4
Faiq <faiq.kaboel@gmail.com>
2019-11-27 07:47:59 +0700

    add admin landing page

---
+++ client/src/views/admin/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="main">
+    <div class="columns is-fullheight main-parent">
+      <ts-sidebar/>
+      <div class="container column is-10">
+        yoloyolo
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import TsSidebar from "../../components/admin/TsSidebar";
+export default {
+  name: 'AdminMain',
+  components: {TsSidebar}
+}
+</script>
+<style scoped>
+#main {
+  min-height: 100%;
+  height: 100%;
+}
+.main-parent {
+  padding: 30px;
+}
+</style>
