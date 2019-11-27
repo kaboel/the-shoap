3fa5cd1255fa2f7b181c4fc134470ee492464d44
Faiq <faiq.kaboel@gmail.com>
2019-11-27 07:48:11 +0700

    add admin sidebar

---
+++ client/src/components/admin/TsSidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <aside class="column is-2 is-fullheight">
+    <p class="menu-label is-hidden-touch">Navigation</p>
+    <ul class="menu-list">
+      <li>
+        <a href="#" class="">
+          <font-awesome-icon :icon="['fa', 'home']"/> Products
+        </a>
+      </li>
+      <li>
+        <a href="#" class="">
+          <font-awesome-icon :icon="['fa', 'link']"/> Types
+        </a>
+      </li>
+      <li>
+        <a href="#" class="">
+          <font-awesome-icon :icon="['fa', 'info']"/> Orders
+        </a>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+  export default {
+    name: "TsSidebar"
+  }
+</script>
+
+<style scoped>
+
+</style>
