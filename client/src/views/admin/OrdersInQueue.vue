be0cd24a730ed47479e9abed1f7604cbc67f365e
Faiq <faiq.kaboel@gmail.com>
2019-12-02 00:37:09 +0700

    add page for orders in queue

---
+++ client/src/views/admin/OrdersInQueue.vue
@@ -0,0 +1,159 @@
+<template>
+  <section>
+    <div class="loading"
+         v-if="allOrders == null">
+      Loading...
+    </div>
+
+    <div class="table-container"
+         v-if="allOrders != null">
+      <table>
+        <thead>
+        <tr>
+          <th>Product ID</th>
+          <th>OD</th>
+          <th>Status</th>
+          <th>Customer Name</th>
+          <th>Email</th>
+          <th>Pick Up / Deliver</th>
+          <th>Order Summary</th>
+          <th/>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="order in allOrders" :key="order._id">
+          <td>
+            <span class="tag">
+              <b>#</b>{{order._id}}
+            </span>
+          </td>
+          <td>
+            {{ formatDate(order.createdAt) }}
+          </td>
+          <td>
+            <div class="control">
+              <b-taglist attached>
+                <b-tag type="is-warning">
+                  <font-awesome-icon :icon="['fa', 'sync-alt']"/>
+                </b-tag>
+                <b-tag type="is-dark">
+                  {{ formatDate(order.updatedAt) }}
+                </b-tag>
+              </b-taglist>
+            </div>
+          </td>
+          <td>
+            {{ order.name }}
+          </td>
+          <td>
+            {{ order.email }}
+          </td>
+          <td>
+            {{ order.address }}
+          </td>
+          <td>
+            <button class="button is-info is-small" @click="sectionTo({parent: 'Orders', child: 'Detail', id: order._id})">
+              View Summary&nbsp;&nbsp;<font-awesome-icon :icon="['fa', 'arrow-right']"/>
+            </button>
+          </td>
+          <td>
+            <button class="button is-success is-small">
+              Complete Order&nbsp;&nbsp;<font-awesome-icon :icon="['fa', 'check']"/>
+            </button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+</template>
+<script>
+  import api from '../../service/api'
+  import {mapState} from 'vuex'
+
+  export default {
+    name: 'OrderInQueue',
+    data () {
+      return {
+        allOrders: null
+      }
+    },
+    computed: mapState(['sectionActive', 'orders', 'types', 'products']),
+    watch: {
+      orders (newVal, oldVal) {
+        this.allOrders = newVal
+      },
+      types (newVal, oldVal) {
+        this.allTypes = newVal
+      },
+      products (newVal, oldVal) {
+        this.allProducts = newVal
+      }
+    },
+    mounted () {
+      this.loadData()
+    },
+    methods: {
+      async loadOrders () {
+        await api.order.getOrderByStatus(false).then(res => {
+          this.$store.dispatch('fillOrders', res.data)
+        }).catch(err => {
+          console.log(err)
+        })
+      },
+
+      async loadProducts () {
+        await api.product.getAllProduct().then(res => {
+          this.$store.dispatch('fillProducts', res.data)
+        }).catch(err => {
+          console.log(err.message)
+        })
+      },
+
+      async loadTypes () {
+        await api.type.getAllTypes().then(res => {
+          this.$store.dispatch('fillTypes', res.data)
+        }).catch(err => {
+          console.log(err.message)
+        })
+      },
+
+      loadData () {
+        this.loadOrders()
+        this.loadProducts()
+        this.loadTypes()
+      },
+
+      formatDate (plain) {
+        let date = new Date(plain)
+        let d = date.getDate()
+        let m = date.getMonth()
+        let y = date.getFullYear()
+
+        return `${m}/${d}/${y}`
+      },
+
+      sectionTo (section) {
+        let store = this.$store
+        store.dispatch('sectionTo', section)
+      }
+    }
+  }
+</script>
+<style scoped>
+  .is-size-xs {
+    font-size: .7rem;
+  }
+  td {
+    vertical-align: middle !important;
+  }
+  .loading {
+    width: 100%;
+    min-height: 75vh;
+    line-height: 75vh;
+    font-size: 1rem;
+    font-weight: bolder;
+    color: #aaa;
+    text-align: center;
+  }
+</style>
